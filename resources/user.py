8adf9dc9048f979cf5269587ca9f3ccbf7bfa680
jdotb <jdotbdotbold@gmail.com>
2021-11-10 18:18:04 -0500

    added password hashing and user model, updated app.py

---
+++ resources/user.py
@@ -0,0 +1,40 @@
+from flask import request
+from flask_restful import Resource
+from http import HTTPStatus
+
+from utils import hash_password
+from models.user import User
+
+class UserListResource(Resource):
+    # POST method
+    def post(self):
+        json_data = request.get_json()
+
+        username = json_data.get('username')
+        email = json_data.get('email')
+        non_hash_password = json_data.get('password')
+
+        # Check whether user exists in db using get_by_username/get_by_email
+        if User.get_by_username(username):
+            return {'message':'username already used'}, HTTPStatus.BAD_REQUEST
+
+        if User.get_by_email(email):
+            return {'message': 'email already used'}, HTTPStatus.BAD_REQUEST
+
+        # create user in db
+        password = hash_password(non_hash_password)
+
+        user = User(
+                username=username,
+                email=email,
+                password=password
+            )
+        user.save()
+
+        data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        }
+
+        return data, HTTPStatus.CREATED
